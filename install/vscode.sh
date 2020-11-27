e3a033ba95cf841302536de893c8a0426a0c786b
Thibault Serti <thibault.serti@gmail.com>
2020-11-27 17:42:32 +0100

    Add Vscode extensions

---
+++ install/vscode.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# ESLint, Prettier
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension esbenp.prettier-vscode
+
+# Shell
+code --install-extension foxundermoon.shell-format
+code --install-extension timonwong.shellcheck
+
+# Docker
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension ms-vscode-remote.remote-containers
+
+# SQL
+code --install-extension mtxr.sqltools
+code --install-extension mtxr.sqltools-driver-sqlite
+
+# Icons and ut
+code --install-extension PKief.material-icon-theme
+code --install-extension tomoki1207.pdf
+code --install-extension GrapeCity.gc-excelviewer
+code --install-extension redhat.vscode-xml
+
+# Programming languages
+code --install-extension golang.go
+code --install-extension ms-vscode.cpptools
+code --install-extension redhat.java
+code --install-extension ms-vscode.powershell
+code --install-extension ms-python.python
+code --install-extension ms-toolsai.jupyter
