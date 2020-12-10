01bc0cefff6d209600382d7aa92f8af8b1fd270a
Thibault Serti <thibault.serti@gmail.com>
2020-12-10 11:49:19 +0100

    Improve permissions management

--- install/conf.sh
+++ install/conf.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 
-
 echo "Copying conf files in the right folders ..."
-cp ../src/.bashrc             ~/.bashrc
-cp ../src/.gitconfig          ~/.gitconfig
-cp ../src/.bash_aliases       ~/.bash_aliases
-cp ../src/.muttrc             ~/.muttrc
-cp ../src/.profile            ~/.profile
-cp ../src/.screenrc           ~/.screenrc
-cp ../src/.tmux.conf.local    ~/.tmux.conf.locl
-cp ../src/.vimrc              ~/.vimrc
-cp ../src/.zshrc              ~/.zshrc
+install --mode=640 src/.bashrc ~/.bashrc
+install --mode=640 src/.gitconfig ~/.gitconfig
+install --mode=640 src/.bash_aliases ~/.bash_aliases
+install --mode=640 src/.muttrc ~/.muttrc
+install --mode=640 src/.profile ~/.profile
+install --mode=640 src/.screenrc ~/.screenrc
+install --mode=640 src/.tmux.conf.local ~/.tmux.conf.locl
+install --mode=640 src/.vimrc ~/.vimrc
+install --mode=640 src/.zshrc ~/.zshrc
 
 mkdir -p ~/.ssh ~/.tmuxinator ~/bin ~/.config ~/.gnupg
+chmod 750 ~/.ssh ~/.tmuxinator ~/bin ~/.config ~/.gnupg
 
-cp -r ../src/tmuxinator       ~
-cp -r ../src/ssh              ~
-cp -r ../src/gnupg            ~
-cp -r ../src/config           ~
-cp -r ../src/bin              ~
-
-chmod +x ~/bin/*
+install --mode=640 src/tmuxinator/* ~/.tmuxinator/
+install --mode=640 src/ssh/* ~/.ssh/
+install --mode=700 src/gnupg/* ~/.gnupg/
+install --mode=750 src/bin/* ~/bin/
+find src/config/ -type f -exec sh -c 'dir="$(cut -d"/" -f3- <<< "$1")"; install --mode=640 "$1" ~/.config/$dir' _ {} \;
